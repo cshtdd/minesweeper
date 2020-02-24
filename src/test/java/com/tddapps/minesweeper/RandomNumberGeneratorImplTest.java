6c27f3d0a9bd2d4e079d95feaf8bcf336e746db8
Camilo Sanchez <camilin87@gmail.com>
2020-02-24 14:30:24 -0500

    Refactor the RandomNumberGenerator tests in a way where more scenarios can be validated

--- src/test/java/com/tddapps/minesweeper/RandomNumberGeneratorImplTest.java
+++ src/test/java/com/tddapps/minesweeper/RandomNumberGeneratorImplTest.java
@@ -4,18 +4,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RandomNumberGeneratorImplTest {
-    @Test
-    void GeneratesNumbersWithinARangeThatRarelyCollapse(){
-        var g = new RandomNumberGeneratorImpl();
-        var l = new ArrayList<Integer>();
+    private final RandomNumberGenerator generator = new RandomNumberGeneratorImpl();
+    private final List<Integer> generatedNumbers = new ArrayList<Integer>();
 
+    RandomNumberGeneratorImplTest(){
         for (int i = 0; i < 10000; i++) {
-            l.add(g.generate(-100, 100));
+            generatedNumbers.add(generator.generate(-100, 100));
         }
+    }
 
-        var uniqueNumbersCount = l.stream().distinct().count();
-        assertTrue(uniqueNumbersCount >= (l.size() * 0.8));
+    @Test
+    void GeneratesNumbersThatRarelyCollapse(){
+        var uniqueNumbersCount = generatedNumbers.stream().distinct().count();
+        assertTrue(uniqueNumbersCount >= (generatedNumbers.size() * 0.8));
     }
 }
