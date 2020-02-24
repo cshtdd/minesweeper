2fcb2f0c75c20ee054d0bc75d1734d6390df4312
Camilo Sanchez <camilin87@gmail.com>
2020-02-24 17:03:07 -0500

    Create flagged and expanded properties for the Cell

--- src/main/java/com/tddapps/minesweeper/Cell.java
+++ src/main/java/com/tddapps/minesweeper/Cell.java
@@ -3,14 +3,18 @@ package com.tddapps.minesweeper;
 class Cell {
     private boolean mine;
     private int surroundingMinesCount;
+    private boolean flagged;
+    private boolean expanded;
 
     public Cell(){
-        this(false, 0);
+        this(false, 0, false, false);
     }
 
-    public Cell(boolean isMine, int surroundingMinesCount) {
+    public Cell(boolean isMine, int surroundingMinesCount, boolean expanded, boolean flagged) {
         this.mine = isMine;
         this.surroundingMinesCount = surroundingMinesCount;
+        this.expanded = expanded;
+        this.flagged = flagged;
     }
 
     public boolean isMine() {
@@ -27,5 +31,21 @@ class Cell {
 
     public void setSurroundingMinesCount(int surroundingMinesCount) {
         this.surroundingMinesCount = surroundingMinesCount;
+    }
+
+    public boolean isFlagged() {
+        return flagged;
+    }
+
+    public void setFlagged(boolean flagged) {
+        this.flagged = flagged;
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
     }
 }
