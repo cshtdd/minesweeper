1e0261741970d3fbad5b4078b39fb8c50c087784
Camilo Sanchez <camilin87@gmail.com>
2020-02-24 18:59:30 -0500

    Create a CellFormatterDefault

---
+++ src/main/java/com/tddapps/minesweeper/CellFormatterDefault.java
@@ -0,0 +1,23 @@
+package com.tddapps.minesweeper;
+
+class CellFormatterDefault implements CellFormatter {
+    @Override
+    public String format(Cell cell) {
+        if (cell.isFlagged()){
+            return "F ";
+        }
+
+        if (cell.isExpanded()){
+            if (cell.isMine()){
+                return "X ";
+            }
+
+            int count = cell.getSurroundingMinesCount();
+            if (count > 0){
+                return String.format("%d ", count);
+            }
+        }
+
+        return "  ";
+    }
+}
---
+++ src/test/java/com/tddapps/minesweeper/CellFormatterDefaultTest.java
@@ -0,0 +1,50 @@
+package com.tddapps.minesweeper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CellFormatterDefaultTest {
+    private final CellFormatter formatter = new CellFormatterDefault();
+
+    @Test
+    void DisplaysAnEmptyStringByDefault(){
+        var cell = new Cell();
+
+        assertEquals("  ", formatter.format(cell));
+    }
+
+    @Test
+    void DisplaysAFlagWhenTheCellHasBeenFlagged(){
+        var cell = new Cell();
+        cell.setFlagged(true);
+
+        assertEquals("F ", formatter.format(cell));
+    }
+
+    @Test
+    void DisplaysTheNumberOfSurroundingMinesWhenExpanded(){
+        var cell = new Cell();
+        cell.setExpanded(true);
+        cell.setSurroundingMinesCount(5);
+
+        assertEquals("5 ", formatter.format(cell));
+    }
+
+    @Test
+    void DisplaysEmptyWhenExpandedButThereAreNoSurroundingMines(){
+        var cell = new Cell();
+        cell.setExpanded(true);
+
+        assertEquals("  ", formatter.format(cell));
+    }
+
+    @Test
+    void DisplayExplosionWhenExpandedButCellHasMine(){
+        var cell = new Cell();
+        cell.setExpanded(true);
+        cell.setMine(true);
+
+        assertEquals("X ", formatter.format(cell));
+    }
+}
