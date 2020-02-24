051e0e80a436d3436b0ad506f89588fdd6c48ee9
Camilo Sanchez <camilin87@gmail.com>
2020-02-24 13:32:18 -0500

    Create a new project

---
+++ src/test/java/ProgramTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProgramTest {
+    @Test
+    void SampleTest(){
+        assertEquals(3, 4 - 1);
+    }
+}
