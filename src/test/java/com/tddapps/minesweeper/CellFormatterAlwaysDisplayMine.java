3bca0cde208e58b1f1dcddd26b5d890e0e64000e
Camilo Sanchez <camilin87@gmail.com>
2020-02-24 19:10:45 -0500

    Simplify the CellFormatterAlwaysDisplayMine

--- src/test/java/com/tddapps/minesweeper/CellFormatterAlwaysDisplayMine.java
+++ src/test/java/com/tddapps/minesweeper/CellFormatterAlwaysDisplayMine.java
@@ -1,22 +1,12 @@
 package com.tddapps.minesweeper;
 
 class CellFormatterAlwaysDisplayMine implements CellFormatter {
-    private final char mineChar;
-
-    CellFormatterAlwaysDisplayMine(){
-        this('M');
-    }
-
-    CellFormatterAlwaysDisplayMine(char mineChar){
-        this.mineChar = mineChar;
-    }
-
     @Override
     public String format(Cell cell) {
-        if (cell.isMine()){
-            return String.format("%s ", mineChar);
-        } else {
+        if (!cell.isMine()) {
             return "  ";
         }
+
+        return "M ";
     }
 }
